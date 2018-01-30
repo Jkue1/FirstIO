36b4c02e3dcf326bcfe2dba23ee4d3a4ece148e1
jkue1 <jkue1@mail.csuchico.edu>
2018-01-29 17:42:02 -0800

    changes the greeting:
    git push

--- hello.cpp
+++ hello.cpp
@@ -1,10 +1,11 @@
+//Author: Joshua Kue
 #include <iostream>
 #include <string>
 
 int main() {
     std::string input;
-    std::cout << "What's your first name?" << std::endl;
+    std::cout << "Hi, my name is Joshua" << std::endl;
     std::cin >> input;
-    std::cout << "Hello, " << input << std::endl;
+    std::cout << "Hello" << input << std::endl;
     return 0;
 }
